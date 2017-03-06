7224d8db446f62c085af5122f0189efdd17e21b0
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2017-03-06 15:15:43 -0800

    Add Skillz Android CS wrappers

---
+++ Pinball/Assets/Scripts/SkillzDelegate.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System;
+
+public class SkillzDelegate : MonoBehaviour {
+
+	// This method is called when a user starts a match from Skillz
+	public void SkillzMatchStarted() {
+		// implement me
+	}
+
+	// This method is called when a user exits the Skillz experience (via Menu -> Exit)
+	public void SkillzExited() {
+		// implement me
+	}
+}
