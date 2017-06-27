0e4f14cb67175d3d02638fb703b45de50691f541
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2017-06-27 14:59:09 -0700

    upload pinball staging android studio project

--- UnityPinballAndroidStudio/Pinball!/build/generated/source/buildConfig/androidTest/debug/com/ZPotatoWu/Pinball/test/BuildConfig.java
+++ UnityPinballAndroidStudio/Pinball!/build/generated/source/buildConfig/androidTest/debug/com/ZPotatoWu/Pinball/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ZPotatoWu.Pinball.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = -1;
-  public static final String VERSION_NAME = "";
+  public static final int VERSION_CODE = 40000001;
+  public static final String VERSION_NAME = "4.0.0.1";
 }
