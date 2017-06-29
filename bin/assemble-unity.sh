6fd99a0f6785801f8eaa9e59c6e0871aac8895b6
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2017-06-29 16:46:23 -0700

    modify assemble-unity

--- bin/assemble-unity.sh
+++ bin/assemble-unity.sh
@@ -1,20 +1,11 @@
 # package SDK release
 GITVERSION=`git describe --match '*.*.*' | sed -e 's,-.*,,'`
 echo GITVERSION is ${GITVERSION}
-SDKDIR=sdk_ios_${GITVERSION}
-
-# auto-document the Unity wrapper
-cd "${WORKSPACE}/SkillzUnityWrapper/UnityPackage/SkillzUnityPackageProject/Assets"
-sed -i '' "s/PLACEHOLDER_SDK_VERSION_FOR_JENKINS/${GITVERSION}/" DoxConfig
-/Applications/Doxygen.app/Contents/Resources/doxygen DoxConfig
-rm DoxConfig
+SDKDIR=sdk_ios_22
 
 # copy the Unity wrapper docs into the packaged SDK
 cd ${WORKSPACE}
 mkdir -p $SDKDIR/Unity
-cp -R "${WORKSPACE}/SkillzUnityWrapper/docs/" $SDKDIR/Unity
-rm -rf "${WORKSPACE}/SkillzUnityWrapper/docs/"
-mv -f "${SDKDIR}/Unity/html" "${SDKDIR}/Unity/docs"
 
 # build the Unity package and copy the needed files to the ouput directory
 . "${WORKSPACE}/SkillzUnityWrapper/create-unity-folder.sh"
