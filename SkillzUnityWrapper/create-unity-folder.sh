b36e335e5f3d54e9a93644afa8a18ec4f22f694f
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2017-06-29 16:23:02 -0700

    bring back to default create unity folder

--- SkillzUnityWrapper/create-unity-folder.sh
+++ SkillzUnityWrapper/create-unity-folder.sh
@@ -4,23 +4,21 @@ WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUPUT_PATH="${WORKDIR}/output"
 UNITY_BIN="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 EXPORT_PATH="${OUPUT_PATH}/skillz_ios.unitypackage"
-PROJECT_PATH="${WORKDIR}/SkillzUnityPackageProject"
+PROJECT_PATH="${WORKDIR}/UnityPackage/SkillzUnityPackageProject"
 ASSETS_PATH="Assets"
-#XCODE_SRC_IN_PATH="${WORKDIR}/XcodeFiles" #probably not needed
-#XCODE_SRC_OUT_PATH="${PROJECT_PATH}/Assets/Skillz/Build/IncludeInXcode" #probably not needed
+XCODE_SRC_IN_PATH="${WORKDIR}/XcodeFiles"
+XCODE_SRC_OUT_PATH="${PROJECT_PATH}/Assets/Skillz/Build/IncludeInXcode"
 
-#probably not needed
-# mkdir "${PROJECT_PATH}/Assets/Plugins"
-# mkdir "${PROJECT_PATH}/Assets/Plugins/iOS"
+mkdir "${PROJECT_PATH}/Assets/Plugins"
+mkdir "${PROJECT_PATH}/Assets/Plugins/iOS"
 
 rm -rf "$OUPUT_PATH"
 mkdir -p "$OUPUT_PATH"
 
-#test
+
 # Copy XcodeFiles into output path with proper directories
-# probably not needed anymore since everything is inside SkillzUnityProject
-# mkdir -p "$XCODE_SRC_OUT_PATH"
-# cp -R "${XCODE_SRC_IN_PATH}"/* "$XCODE_SRC_OUT_PATH"
+mkdir -p "$XCODE_SRC_OUT_PATH"
+cp -R "${XCODE_SRC_IN_PATH}"/* "$XCODE_SRC_OUT_PATH"
 
 
 # Create Unity package from blank project
