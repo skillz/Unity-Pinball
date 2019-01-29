3f76ac7144c6b908572ec41edeca84a8ed38b195
Jerry Lin <jlin@skillz.com>
2019-01-29 15:59:27 -0800

    SDK Integration: Prepwork for SDK Integration
    Prepwork to prepare Unity-Pinball for integration of the Skillz SDK. This includes:
    - Changing the Pause button to a Forfeit button so that eventually the player can forfeit a match.
    - Changing the Play button in the main menu so that it will eventually launch Skillz.
    - Remove the Reset button.

---
+++ Pinball/Assets/Scripts/ManMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ManMenuManager : MonoBehaviour
+{
+    public void LaunchSkillz()
+    {
+        // TODO: Launch Skillz
+        Debug.Log("TODO: Launch Skillz");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
